6af7c771339816160b3f6ec19259bdec0c9d5a0d
SRDRGNCR <serdarguncur@gmail.com>
2021-11-03 15:32:21 +0300

    Son Hali

--- introductionPython.py
+++ introductionPython.py
@@ -17,8 +17,17 @@ bol   = sayi1 / sayi2
 kalan = sayi1 % sayi2
 kalan2= sayi1 // sayi2
 
-print(5*"#"+karakter+5*"#")       # Print fonksiyonu çıktı vermek için kullanılır
-                                  # Operatorler ile string veri tiplerinde işlemler yapılabilir
+print(""" 
+        \rPYTHON NOTLARIM :\n1-VERİ TİPLERİ\t
+        \r2-DEĞİŞKENLER\n\r3-PRİNT FONKSİYONU\t\r4-STRİNG VERİ TİPLERİ\n""")# Print fonksiyonu çıktı vermek için kullanılır
+                                                                                                           # Operatorler ile string veri tiplerinde işlemler yapılabilir
+                                                                                                           # Özel anlam taşıyan string karakterlerikullanmak için kaçış dizeleri kullanılır.     
 
-print(""" işlem sonuçları """,topla,cikar,carp,bol,kalan,kalan2)
+                                                                                                            # \ = tınrak işarteleri kullanımında kullanılır
+                                                                                                            # \n = satır başı
+                                                                                                            # \t = sağ tarafa doğru 4 satır boşuk bırakır
+                                                                                                            # \r = Satır başı
+                                                                                                            # \v = düşey sekme
+                                                                                                            # \b = imleç kaydırma
+print(""" işlem sonuçları """,topla,cikar,carp,bol,kalan,kalan2,"\a")
 
